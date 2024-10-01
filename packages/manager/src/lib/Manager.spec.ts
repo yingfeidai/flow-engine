c09efcd1a43c4e6cf3bbf13814785cb27b915968
dyf96 <yingfei.dai96@gmail.com>
2024-10-01 11:56:14 +0900

    feat: ✨ init manager pkg

---
+++ packages/manager/src/lib/Manager.spec.ts
@@ -0,0 +1,7 @@
+import { manager } from './Manager';
+
+describe('manager', () => {
+  it('should work', () => {
+    expect(manager()).toEqual('manager');
+  });
+});
---
+++ packages/manager/src/lib/Manager.ts
@@ -0,0 +1,3 @@
+export function manager(): string {
+  return 'manager';
+}
