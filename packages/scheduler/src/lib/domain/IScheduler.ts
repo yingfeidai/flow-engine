eac9243d5c5ba7bd9ccb0035fd1dc522469acd76
dyf96 <yingfei.dai96@gmail.com>
2024-10-10 14:07:53 +0900

    fix: 🐛 use ISchedulerProperties

--- packages/scheduler/src/lib/domain/IScheduler.ts
+++ packages/scheduler/src/lib/domain/IScheduler.ts
@@ -7,8 +7,8 @@ export interface ISchedulerProperties<TScheduleType = SchedulerTypeEnum>
   type: TScheduleType;
 }
 
-export interface IScheduler {
-  schedule(id: string, scheduleTime: Date): Promise<void>;
+export interface IScheduler<TProperties = ISchedulerProperties> {
+  schedule(scheduler: TProperties): Promise<void>;
   cancelScheduled(id: string): Promise<void>;
   getScheduled(): Promise<{ id: string; scheduleTime: Date }[]>;
   pauseScheduled(id: string): Promise<void>;
